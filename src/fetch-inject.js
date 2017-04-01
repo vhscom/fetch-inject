e50f2a38a336b02eb7487629bf6b9fae4d283b48
vhs <vhsdev@tutanota.com>
2017-04-01 22:11:20 +0800

    refactor(fetchInject): improve code style
    remove branch logic, do callback in injector, rename identifiers

--- src/fetch-inject.js
+++ src/fetch-inject.js
@@ -10,9 +10,9 @@ export default function (urls) {
   urls.forEach(url => deferreds.push(
     window.fetch(url).then(res => {
       return [res.clone().text(), res.blob()]
-    }).then(kvArrays => {
-      return Promise.all(kvArrays).then(kvArray => {
-        resources.push({ text: kvArray[0], type: kvArray[1].type })
+    }).then(promises => {
+      return Promise.all(promises).then(resolved => {
+        resources.push({ text: resolved[0], type: resolved[1].type })
       })
     })
   ))
@@ -20,8 +20,9 @@ export default function (urls) {
   return Promise.all(deferreds).then(() => {
     resources.forEach(resource => {
       thenables.push({ then: resolve => {
-        const tag = resource.type === 'text/css' ? 'style' : 'script'
-        injectHead(window, document, tag, resource, resolve())
+        resource.type === 'text/css'
+          ? injectHead(window, document, 'style', resource, resolve)
+          : injectHead(window, document, 'script', resource, resolve)
       }})
     })
     return Promise.all(thenables)
