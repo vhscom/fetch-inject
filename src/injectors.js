25118606da5d2ef3a8a930e5278fcbcfb5137495
vhs <vhsdev@tutanota.com>
2017-05-01 22:16:04 +0800

    revert: "fix(fetchInject,injectors): Allow optional Content-Type header parameter"
    This reverts commit 5f112df9552168f2f7b38484bb44fe4e3b4e4f08.

--- src/injectors.js
+++ src/injectors.js
@@ -1,1 +1,1 @@
-export const head = (function(i,s,o,g,r,a,m){a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.type=g.blob.type.split(';')[0];a.appendChild(s.createTextNode(g.text));a.onload=r(g);m?m.parentNode.insertBefore(a,m):s.head.appendChild(a)}) // eslint-disable-line
+export const head = (function(i,s,o,g,r,a,m){a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.type=g.blob.type;a.appendChild(s.createTextNode(g.text));a.onload=r(g);m?m.parentNode.insertBefore(a,m):s.head.appendChild(a)}) // eslint-disable-line
