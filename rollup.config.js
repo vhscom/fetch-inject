a14bc0cfda2a085f5a44f2bf4e80cc9fe3aa518a
vhs <vhsdev@tutanota.com>
2017-05-15 00:07:32 +0800

    build(rollup): learn how to spell, chief

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ const config = {
   sourceMap: false,
   plugins: [
     license({
-      banner: `Fetch Inject v<%= pkg.version %>\nCopyright (c) <%= moment().format('YYYY') %> VHS\n@licence ISC`
+      banner: `Fetch Inject v<%= pkg.version %>\nCopyright (c) <%= moment().format('YYYY') %> VHS\n@license ISC`
     })
   ]
 }
